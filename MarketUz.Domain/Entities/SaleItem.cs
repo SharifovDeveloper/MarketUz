532b9c16b6649440aa91ac70399cdf12d642e6cf
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-01-09 22:22:03 +0500

    cleared SaleItemEntity

--- MarketUz.Domain/Entities/SaleItem.cs
+++ MarketUz.Domain/Entities/SaleItem.cs
@@ -4,13 +4,10 @@ namespace MarketUz.Domain.Entities
 {
     public class SaleItem : EntityBase
     {
-
         public int Quantity { get; set; }
         public decimal UnitPrice { get; set; }
-
         public int ProductId { get; set; }
         public Product Product { get; set; }
-
         public int SaleId { get; set; }
         public Sale Sale { get; set; }
     }
