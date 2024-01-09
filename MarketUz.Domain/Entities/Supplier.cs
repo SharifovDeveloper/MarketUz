623d46c8d6987bdcc669e63353d843ed11ecd1b0
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-01-09 22:22:30 +0500

    cleared SupplierEntity

--- MarketUz.Domain/Entities/Supplier.cs
+++ MarketUz.Domain/Entities/Supplier.cs
@@ -4,12 +4,10 @@ namespace MarketUz.Domain.Entities
 {
     public class Supplier : EntityBase
     {
-
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public string Company { get; set; }
-
         public virtual ICollection<Supply> Supplies { get; set; }
     }
 }
