c39429ca96160a7ef019e904ae00170a33cf3d7a
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-02-06 22:32:10 +0500

    Avoid warning to CustomerDto

--- MarketUz.Domain/DTOs/Customer/CustomerDto.cs
+++ MarketUz.Domain/DTOs/Customer/CustomerDto.cs
@@ -5,8 +5,8 @@ namespace MarketUz.Domain.DTOs.Customer
     public class CustomerDto
     {
         public int Id { get; set; }
-        public string FullName { get; set; }
-        public string Phone { get; set; }
-        public virtual ICollection<SaleDto> Sales { get; set; }
+        public string? FullName { get; set; }
+        public string? Phone { get; set; }
+        public virtual ICollection<SaleDto>? Sales { get; set; }
     }
 }
