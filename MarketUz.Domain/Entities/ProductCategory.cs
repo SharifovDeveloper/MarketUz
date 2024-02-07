8d91ff0845a0a574868373c81278cb08c8abf065
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-02-07 22:32:13 +0500

    Avoid warning to ProductCategory

--- MarketUz.Domain/Entities/ProductCategory.cs
+++ MarketUz.Domain/Entities/ProductCategory.cs
@@ -5,9 +5,9 @@ namespace MarketUz.Domain.Entities
     public class ProductCategory : EntityBase
     {
         public int ProductId { get; set; }
-        public Product Product { get; set; }
+        public Product? Product { get; set; }
 
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
     }
 }
