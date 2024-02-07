c443b464883b14c2e7a5d04dfd8bdfddf12d7d6c
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-02-07 22:33:36 +0500

    Avoid warning to Supplier

--- MarketUz.Domain/Entities/Supplier.cs
+++ MarketUz.Domain/Entities/Supplier.cs
@@ -4,10 +4,10 @@ namespace MarketUz.Domain.Entities
 {
     public class Supplier : EntityBase
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Company { get; set; }
-        public virtual ICollection<Supply> Supplies { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string PhoneNumber { get; set; } = "+998949323212";
+        public string? Company { get; set; }
+        public virtual ICollection<Supply>? Supplies { get; set; }
     }
 }
