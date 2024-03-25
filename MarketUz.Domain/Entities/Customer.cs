a17ea657ab30df9df4e36d47e1e37df99fd9e5a9
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-03-25 13:52:56 +0500

    Cleared unnecessary lines

--- MarketUz.Domain/Entities/Customer.cs
+++ MarketUz.Domain/Entities/Customer.cs
@@ -4,7 +4,6 @@ namespace MarketUz.Domain.Entities
 {
     public class Customer : EntityBase
     {
-
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string PhoneNumber { get; set; } = "+998949336200";
