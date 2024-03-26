3442aebdadb7e7412c08ce937510d9352b7a49d9
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-03-26 16:39:42 +0500

    Added CategoryResourceParameters

---
+++ MarketUz.Domain/ResourceParameters/CustomerResourceParameters.cs
@@ -0,0 +1,18 @@
+﻿namespace MarketUz.Domain.ResourceParameters
+{
+    public  class CustomerResourceParameters
+    {
+        private const int MaxPageSize = 25;
+
+        public string? SearchString { get; set; }
+        public string OrderBy { get; set; } = "firstname";
+
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 15;
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+        }
+    }
+}
