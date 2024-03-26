2704fdfdcde70e42e550640b9b24a3ebd52fcb3b
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-03-26 16:50:06 +0500

    Update ProductResourceParameters

--- MarketUz.Domain/ResourceParameters/ProductResourceParameters.cs
+++ MarketUz.Domain/ResourceParameters/ProductResourceParameters.cs
@@ -1,23 +1,13 @@
-﻿namespace MarketUz.ResourceParameters
-{
-    public class ProductResourceParameters
-    {
-        private const int MaxPageSize = 25;
+﻿using MarketUz.Domain.ResourceParameters;
 
+namespace MarketUz.ResourceParameters
+{
+    public class ProductResourceParameters : ResourceParametersBase
+    { 
         public int? CategoryId { get; set; }
-        public string? SearchString { get; set; }
         public decimal? Price { get; set; }
         public decimal? PriceLessThan { get; set; }
         public decimal? PriceGreaterThan { get; set; }
-        public string OrderBy { get; set; } = "name";
-
-        public int PageNumber { get; set; } = 1;
-
-        private int _pageSize = 15;
-        public int PageSize
-        {
-            get => _pageSize;
-            set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
-        }
+        public override string OrderBy { get; set; } = "name";
     }
 }
