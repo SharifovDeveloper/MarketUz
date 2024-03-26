b736c13cafc1f9ea329517008abc37bce4801b22
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-03-26 17:12:00 +0500

    Completed SaleItemResourceParameters

--- MarketUz.Domain/ResourceParameters/SaleItemResourceParameters.cs
+++ MarketUz.Domain/ResourceParameters/SaleItemResourceParameters.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MarketUz.Domain.ResourceParameters
+﻿namespace MarketUz.Domain.ResourceParameters
 {
-    internal class SaleItemResourceParameters
+    public class SaleItemResourceParameters : ResourceParametersBase
     {
+        public override string OrderBy { get; set; } = "id";
+        public int? ProductId { get; set; }
+        public int? SaleId { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public decimal? UnitPriceLessThan { get; set; }
+        public decimal? UnitPriceGreaterThan { get; set; }
     }
 }
