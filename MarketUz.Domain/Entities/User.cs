325793005ea45548e335e656329d26299c3df349
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-03-26 11:14:19 +0500

    Added Authentication controller

---
+++ MarketUz.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using MarketUz.Domain.Common;
+
+namespace MarketUz.Domain.Entities
+{
+    public class User : EntityBase
+    {
+        public string Name { get; set; }
+        public string? Phone { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
