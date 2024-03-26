61a9bb2d68a8779f35ef260835fe8a71355424f2
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-03-26 17:09:52 +0500

    Added all classes for Resource parameters

---
+++ MarketUz.Domain/ResourceParameters/SupplierResourceParameters.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketUz.Domain.ResourceParameters
+{
+    internal class SupplierResourceParameters
+    {
+    }
+}
