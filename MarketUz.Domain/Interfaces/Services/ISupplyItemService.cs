c335185d24b4c2dc1882bd81c9d3ab2a2cde1264
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-03-27 15:59:05 +0500

    Updated ISupplyItemService

--- MarketUz.Domain/Interfaces/Services/ISupplyItemService.cs
+++ MarketUz.Domain/Interfaces/Services/ISupplyItemService.cs
@@ -1,10 +1,13 @@
 ﻿using MarketUz.Domain.DTOs.SupplyItem;
+using MarketUz.Domain.ResourceParameters;
+using MarketUz.Domain.Responses;
 
 namespace MarketUz.Domain.Interfaces.Services
 {
     public interface ISupplyItemService
     {
-        IEnumerable<SupplyItemDto> GetSupplyItems();
+        IEnumerable<SupplyItemDto> GetAllSupplyItems();
+        GetBaseResponse<SupplyItemDto> GetSupplyItems(SupplyItemResourceParameters supplyItemResourceParameters);
         SupplyItemDto? GetSupplyItemById(int id);
         SupplyItemDto CreateSupplyItem(SupplyItemForCreateDto supplyItemToCreate);
         void UpdateSupplyItem(SupplyItemForUpdateDto supplyItemToUpdate);
