259c88ff3f0cce30058201c39b859ce812605cb8
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-03-27 16:59:30 +0500

    Added UserConfiguration

---
+++ MarketUz.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using MarketUz.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketUz.Infrastructure.Persistence.Configurations
+{
+    internal class UserEntityConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable(nameof(User));
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(c => c.Phone)
+                .HasMaxLength(255);
+
+            builder.Property(c => c.Login)
+                .IsRequired()
+                .HasMaxLength(255)
+                .HasAnnotation("MinLength", 8);
+
+            builder.Property(c => c.Password)
+                .IsRequired()
+                .HasMaxLength(255)
+                .HasAnnotation("MinLength", 8);
+        }
+    }
+}
