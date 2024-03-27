c792b55e6c1cd2c300c5e5b2a0ae9a569dbbeb37
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-03-27 14:14:29 +0500

    Update SupplierDto

--- MarketUz.Domain/DTOs/Supplier/SupplierDto.cs
+++ MarketUz.Domain/DTOs/Supplier/SupplierDto.cs
@@ -1,9 +1,12 @@
-﻿namespace MarketUz.Domain.DTOs.Supplier
+﻿using MarketUz.Domain.DTOs.Supply;
+
+namespace MarketUz.Domain.DTOs.Supplier
 {
-    public class SupplierDto
-    {
-        public int Id { get; set; }
-        public string? FullName { get; set; }
-        public string? Phone { get; set; }
-    }
+    public record SupplierDto(
+       int Id,
+       string FirstName,
+       string LastName,
+       string PhoneNumber,
+       string Company,
+       ICollection<SupplyDto> Supplies);
 }
