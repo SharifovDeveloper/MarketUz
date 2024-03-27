d82906eb4df588fd63fbccec4da0057d8b02021f
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-03-27 16:16:38 +0500

    Updated SaleItemMappings

--- MarketUz.Domain/Mappings/SaleItemMappings.cs
+++ MarketUz.Domain/Mappings/SaleItemMappings.cs
@@ -8,8 +8,10 @@ namespace MarketUz.Domain.Mappings
     {
         public SaleItemMappings()
         {
-            CreateMap<SaleItem, SaleItemDto>();
             CreateMap<SaleItemDto, SaleItem>();
+            CreateMap<SaleItem, SaleItemDto>()
+                .ForCtorParam(nameof(SaleItemDto.TotalDue), x => x.MapFrom(q => q.Quantity * q.UnitPrice))
+                .ForCtorParam(nameof(SaleItemDto.ProductName), x => x.MapFrom(q => q.Product.Name));
             CreateMap<SaleItemForCreateDto, SaleItem>();
             CreateMap<SaleItemForUpdateDto, SaleItem>();
         }
