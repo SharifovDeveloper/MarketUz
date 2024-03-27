43fb7e5df3f02d09af8ee88aa89c3a2fc99a7495
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-03-27 15:59:57 +0500

    Udpated SupplyItemService

--- MarketUzService/SupplyItemService.cs
+++ MarketUzService/SupplyItemService.cs
@@ -1,10 +1,11 @@
 ﻿using AutoMapper;
 using MarketUz.Domain.DTOs.SupplyItem;
 using MarketUz.Domain.Entities;
-using MarketUz.Domain.Exceptions;
 using MarketUz.Domain.Interfaces.Services;
+using MarketUz.Domain.Pagination;
+using MarketUz.Domain.ResourceParameters;
+using MarketUz.Domain.Responses;
 using MarketUz.Infrastructure.Persistence;
-using Microsoft.Extensions.Logging;
 
 namespace DiyorMarket.Services
 {
@@ -12,31 +13,79 @@ namespace DiyorMarket.Services
     {
         private readonly IMapper _mapper;
         private readonly MarketUzDbContext _context;
-        private readonly ILogger<SupplyItemService> _logger;
 
-        public SupplyItemService(IMapper mapper, MarketUzDbContext context, ILogger<SupplyItemService> logger)
+        public SupplyItemService(IMapper mapper, MarketUzDbContext context)
         {
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
             _context = context ?? throw new ArgumentNullException(nameof(context));
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
-        public IEnumerable<SupplyItemDto> GetSupplyItems()
+        public GetBaseResponse<SupplyItemDto> GetSupplyItems(SupplyItemResourceParameters supplyItemResourceParameters)
+        {
+            var query = _context.SupplyItems.AsQueryable();
+
+            if (supplyItemResourceParameters.ProductId is not null)
+            {
+                query = query.Where(x => x.ProductId == supplyItemResourceParameters.ProductId);
+            }
+
+            if (supplyItemResourceParameters.SupplyId is not null)
+            {
+                query = query.Where(x => x.SupplyId == supplyItemResourceParameters.SupplyId);
+            }
+
+            if (supplyItemResourceParameters.UnitPrice is not null)
+            {
+                query = query.Where(x => x.UnitPrice == supplyItemResourceParameters.UnitPrice);
+            }
+
+            if (supplyItemResourceParameters.UnitPriceLessThan is not null)
+            {
+                query = query.Where(x => x.UnitPrice < supplyItemResourceParameters.UnitPriceLessThan);
+            }
+
+            if (supplyItemResourceParameters.UnitPriceGreaterThan is not null)
+            {
+                query = query.Where(x => x.UnitPrice > supplyItemResourceParameters.UnitPriceGreaterThan);
+
+            }
+
+            if (!string.IsNullOrEmpty(supplyItemResourceParameters.OrderBy))
+            {
+                query = supplyItemResourceParameters.OrderBy.ToLowerInvariant() switch
+                {
+                    "id" => query.OrderBy(x => x.Id),
+                    "iddesc" => query.OrderByDescending(x => x.Id),
+                    "unitprice" => query.OrderBy(x => x.UnitPrice),
+                    "unitpricedesc" => query.OrderByDescending(x => x.UnitPrice),
+                    "saleid" => query.OrderBy(x => x.SupplyId),
+                    "saleiddesc" => query.OrderByDescending(x => x.SupplyId),
+                    "productid" => query.OrderBy(x => x.ProductId),
+                    "productiddesc" => query.OrderByDescending(x => x.ProductId),
+                    _ => query.OrderBy(x => x.Id),
+                };
+            }
+
+            var supplyItems = query.ToPaginatedList(supplyItemResourceParameters.PageSize, supplyItemResourceParameters.PageNumber);
+
+            var supplyItemDtos = _mapper.Map<List<SupplyItemDto>>(supplyItems);
+
+            var paginatedResult = new PaginatedList<SupplyItemDto>(supplyItemDtos, supplyItems.TotalCount, supplyItems.CurrentPage, supplyItems.PageSize);
+
+            return paginatedResult.ToResponse();
+        }
+
+        public IEnumerable<SupplyItemDto> GetAllSupplyItems()
         {
             var supplyItems = _context.SupplyItems.ToList();
 
-            var supplyItemDtos = _mapper.Map<IEnumerable<SupplyItemDto>>(supplyItems);
-
-            return supplyItemDtos;
+            return _mapper.Map<IEnumerable<SupplyItemDto>>(supplyItems) ?? Enumerable.Empty<SupplyItemDto>();
         }
 
         public SupplyItemDto? GetSupplyItemById(int id)
         {
             var supplyItem = _context.SupplyItems.FirstOrDefault(x => x.Id == id);
-            if (supplyItem is null)
-            {
-                throw new EntityNotFoundException($"SupplyItem with id: {id} not found");
-            }
+
             var supplyItemDto = _mapper.Map<SupplyItemDto>(supplyItem);
 
             return supplyItemDto;
