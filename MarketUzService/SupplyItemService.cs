921aac8ecb44e989facd982a03a553013f2eef08
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-02 23:32:43 +0500

    Corrected SupplyItemService

--- MarketUzService/SupplyItemService.cs
+++ MarketUzService/SupplyItemService.cs
@@ -1,11 +1,11 @@
 ﻿using AutoMapper;
-using MarketUz.Domain.DTOs.SupplyItem;
 using MarketUz.Domain.Entities;
 using MarketUz.Domain.Interfaces.Services;
 using MarketUz.Domain.Pagination;
 using MarketUz.Domain.ResourceParameters;
 using MarketUz.Domain.Responses;
 using MarketUz.Infrastructure.Persistence;
+using Inflow.Core.SupplyItem;
 
 namespace DiyorMarket.Services
 {
